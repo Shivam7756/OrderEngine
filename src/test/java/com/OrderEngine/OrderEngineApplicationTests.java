deee4c4c6f401938bca80bdac92f9b517e1e3b96
Shivam Prakash Mishra <shivambit65@gmail.com>
2026-03-09 21:01:10 +0530

    initial commits

---
+++ src/test/java/com/OrderEngine/OrderEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OrderEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
